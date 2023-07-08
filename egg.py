db6899dbc74f77296b73935c7f455423a03912db
Annapurna Shobitha S <74901536+annapurna2003@users.noreply.github.com>
2023-07-08 22:50:31 +0530

    Update egg.py

--- egg.py
+++ egg.py
@@ -6,7 +6,7 @@ canvas_width = 800
 canvas_height = 400
 
 root = Tk()
-root.title("Egg Catcher")
+root.title("Egg Catcher game")
 c = Canvas(root, width=canvas_width, height=canvas_height, background="deep sky blue")
 c.create_rectangle(-5, canvas_height-100, canvas_width+5, canvas_height+5, fill="sea green", width=0)
 c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
